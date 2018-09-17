84a28cb1c41f310622ae19f5327bc43b1ece8fbd
Emmett Hasley <Ehasley1136@jh-mba158-06.d214.org>
2018-09-17 11:35:06 -0500

    Initial Commit

---
+++ guessANumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  guessANumber
+//
+//  Created by Emmett Hasley on 9/17/18.
+//  Copyright © 2018 John Heresy High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
